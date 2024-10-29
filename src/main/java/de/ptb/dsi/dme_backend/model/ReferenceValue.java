ef01c537637d2309893ab08461e9725c6e374d42
MoritzGafert <moritz.gafert@ptb.de>
2024-10-29 19:07:04 +0100

    Implemented constructors for ReferenceValue

--- src/main/java/de/ptb/dsi/dme_backend/model/ReferenceValue.java
+++ src/main/java/de/ptb/dsi/dme_backend/model/ReferenceValue.java
@@ -5,4 +5,10 @@ import lombok.Data;
 @Data
 public class ReferenceValue {
     private SiReal siReal;
+
+    public ReferenceValue(){}
+
+    public ReferenceValue(SiReal siReal){
+        this.siReal = siReal;
+    }
 }
