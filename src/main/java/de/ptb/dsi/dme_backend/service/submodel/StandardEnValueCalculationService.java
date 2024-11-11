2dfa94b1e3267e40a5ff789a3ba6106231436c14
David.Urban <david.urban@ptb.de>
2024-11-11 15:18:33 +0100

    implemented StandardEnValueCalculationService methods and some Unittests

--- src/main/java/de/ptb/dsi/dme_backend/service/submodel/StandardEnValueCalculationService.java
+++ src/main/java/de/ptb/dsi/dme_backend/service/submodel/StandardEnValueCalculationService.java
@@ -1,18 +1,72 @@
 package de.ptb.dsi.dme_backend.service.submodel;
 
 
+import de.ptb.dsi.dme_backend.model.BilateralEnValue;
 import de.ptb.dsi.dme_backend.model.EnValue;
 import de.ptb.dsi.dme_backend.model.ReferenceValue;
 import de.ptb.dsi.dme_backend.model.SiReal;
 import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.List;
+import java.lang.Math;
 
 @Service
 public class StandardEnValueCalculationService implements IEnValueCalculationService {
 
     @Override
     public HashMap<String, EnValue> calculateEnValue(HashMap<String, SiReal> contributionMeasuredValues, ReferenceValue referenceValue, List<String> outlierContributions) {
-        return null;
+        //create Hashmap for output
+        HashMap<String, EnValue> EnValues = new HashMap<>();
+
+        //loop through list of contributionMeasuredValues and calculate correlated/uncorrelated EnValue
+        for (String key : contributionMeasuredValues.keySet()) {
+            SiReal value = contributionMeasuredValues.get(key);
+            EnValue enValue;
+
+            //if Outlier use uncorrelated EnValue, otherwise use correlated enValue
+            if(outlierContributions.contains(key)) {
+                 enValue = calculateUncorrelatedEnValue(value, referenceValue);
+            } else {
+                enValue = calculateCorrelatedEnValue(value, referenceValue);
+            }
+            EnValues.put(key, enValue);
+        }
+        return EnValues;
     }
+
+    public EnValue calculateCorrelatedEnValue(SiReal value, ReferenceValue refValue) {
+        double x = value.getValue();
+        double u_x = value.getExpUnc().getUncertainty();
+        double xRef = refValue.getSiReal().getValue();
+        double u_xRef = refValue.getSiReal().getExpUnc().getUncertainty();
+        //TODO check if converage is 95 %
+
+        double En = Math.abs((x - xRef) / Math.sqrt(u_x*u_x - u_xRef*u_xRef));
+
+        return new EnValue(new SiReal(En), "correlated EnValue");
+    }
+
+    public EnValue calculateUncorrelatedEnValue(SiReal value, ReferenceValue refValue) {
+        double x = value.getValue();
+        double u_x = value.getExpUnc().getUncertainty();
+        double xRef = refValue.getSiReal().getValue();
+        double u_xRef = refValue.getSiReal().getExpUnc().getUncertainty();
+        //TODO check if converage is 95 %
+
+        double En = Math.abs((x - xRef) / Math.sqrt(u_x*u_x + u_xRef*u_xRef));
+
+        return new EnValue(new SiReal(En), "uncorrelated EnValue");
+    }
+
+    public void checkForNullSiReal(SiReal siReal){
+        if(siReal.getValue() == null){
+            throw new NullPointerException("Value in SiReal cannot be null.");
+        }
+    }
+    public void checkForNullInSiExpUnc(SiReal siReal){
+        if(siReal.getExpUnc().getUncertainty() == null){
+            throw new NullPointerException("Uncertainty in ExpUnc cannot be null.");
+        }
+    }
+
 }
---
+++ src/test/java/de/ptb/dsi/dme_backend/service/StandardEnValueCalculationServiceTests.java
@@ -0,0 +1,89 @@
+package de.ptb.dsi.dme_backend.service;
+
+import de.ptb.dsi.dme_backend.model.*;
+import de.ptb.dsi.dme_backend.service.submodel.StandardEnValueCalculationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class StandardEnValueCalculationServiceTests {
+
+    private HashMap<String, SiReal> contributionMeasuredValues;
+    private StandardEnValueCalculationService standardEnValueCalculationService;
+    private List<String> outlierContributions;
+
+    @BeforeEach
+    void init(){
+        standardEnValueCalculationService = new StandardEnValueCalculationService();
+
+        //Example Values
+        contributionMeasuredValues = new HashMap<>();
+        contributionMeasuredValues.put("1", new SiReal(22.11, new SiExpandedUnc(1.0, 2)));
+        contributionMeasuredValues.put("2", new SiReal(22.22, new SiExpandedUnc(1.0, 2)));
+        contributionMeasuredValues.put("3", new SiReal(23.40, new SiExpandedUnc(0.5, 2)));
+
+        outlierContributions = Arrays.asList("1", "3");
+    }
+
+    @Test
+    public void testOutlierCalculation(){
+
+        ReferenceValue referenceValue = new ReferenceValue(new SiReal(22.9883333,
+                new SiExpandedUnc(0.40824829, 2)));
+        HashMap<String, EnValue> enValues = standardEnValueCalculationService.calculateEnValue(
+                contributionMeasuredValues, referenceValue, outlierContributions);
+
+        assertEquals(enValues.get("1").getMethod(), "uncorrelated EnValue");
+        assertEquals(enValues.get("2").getMethod(), "correlated EnValue");
+        assertEquals(enValues.get("3").getMethod(), "uncorrelated EnValue");
+    }
+
+    @Test
+    public void testCorrelatedEnValueCalculation(){
+
+        //creating Inputs
+
+        ReferenceValue referenceValue = new ReferenceValue(new SiReal(22.9883333,
+                new SiExpandedUnc(0.40824829, 2)));
+
+        // Formatting the result
+        EnValue expectedResult = new EnValue(new SiReal(0.962165959), "correlated EnValue");
+
+        // calculating correlated EnValue
+        EnValue result = standardEnValueCalculationService.calculateCorrelatedEnValue(
+                contributionMeasuredValues.get("1"), referenceValue);
+
+        double epsilon = 0.00001;
+        assertEquals(expectedResult.getEnValueRaw().getValue(), result.getEnValueRaw().getValue(), epsilon);
+
+    }
+
+    @Test
+    public void testUncorrelatedEnValueCalculation(){
+
+        //creating Inputs
+
+        ReferenceValue referenceValue = new ReferenceValue(new SiReal(22.1650000,
+                new SiExpandedUnc(0.707106781, 2)));
+
+        // Formatting the result
+        EnValue expectedResult = new EnValue(new SiReal(1.4260552), "uncorrelated EnValue");
+
+        // calculating correlated EnValue
+        EnValue result = standardEnValueCalculationService.calculateUncorrelatedEnValue(
+                contributionMeasuredValues.get("3"), referenceValue);
+
+        double epsilon = 0.00001;
+        assertEquals(expectedResult.getEnValueRaw().getValue(), result.getEnValueRaw().getValue(), epsilon);
+    }
+
+
+}
