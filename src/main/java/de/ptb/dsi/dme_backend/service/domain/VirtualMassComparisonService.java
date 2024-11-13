4c6a68870d6d189f732715c11a8874933df3c601
David.Urban <david.urban@ptb.de>
2024-11-13 11:12:42 +0100

    added some comments

--- src/main/java/de/ptb/dsi/dme_backend/service/domain/VirtualMassComparisonService.java
+++ src/main/java/de/ptb/dsi/dme_backend/service/domain/VirtualMassComparisonService.java
@@ -6,6 +6,22 @@ public class VirtualMassComparisonService implements IComparisonEvaluationServic
     private ComparisonDataModel comparisonDataModel;
     @Override
     public String evaluateComparison(String inputJson) {
+
+        // 1) Daten einlesen
+        // List<ParticipantMeasuredValue> {"1": SiReal, "2": SiReal, "3": SiReal}
+
+        // weigehted mean und en werte ohne outlier berechnen
+
+        // List<String> Outlier : Id vom participantMeasuredResult | Anfang leer und wird daufgefüllt nach jedem durchgang
+        // 2.0) Neue List<ParticipantMeasuredValue> ohne Outlier -> neuer Service?
+        // 2.1) weighted Mean berechnen | calculateReferenceValue(List<ParticipantMeasuredValue> participantMeasuredValueValue)
+        // 3) Enwerte berechenen | calculateEnValue(HashMap<String, SiReal> contributionMeasuredValues, ReferenceValue referenceValue, List<String> outlierContributions)
+        // 4) Consistency check | List<ConsistencyCheckResult> |(En >1 -> Outlier) -> z.B. Id "2", "3" hat EnWert >1 -> Outlier Liste und neu Referenzwert
+        // 5) Outlier aussortieren -> OutlierExclusionService -> List<String> outlierContributions
+
+        // Output Report genereieren
+
+
         return null;
     }
 }
--- src/main/java/de/ptb/dsi/dme_backend/service/submodel/DecisionProcessingService.java
+++ src/main/java/de/ptb/dsi/dme_backend/service/submodel/DecisionProcessingService.java
@@ -12,6 +12,8 @@ public class DecisionProcessingService {
 //        -	Alice defines, if the decision processing is manual (she provides the ID of a participant to be removed) or if it should be processed automatically based in the latest ConsistencyCheckResult
 //        -	If an outlier is removed, the ID of the outlier is written to AnalysisOutput.
 //        -	If a participant is defined as outlier, a new AnalysisOutput is added to ComparisonDataModel and the outlier is removed from the list ParticipantMeasuredValuesContributing and put to list ParticipantMeasuredValuesOutliers
-     return true;
+
+        //suche größten En Wert, der inkonsitent ist und sortiere ihn aus
+        return true;
     }
 }
